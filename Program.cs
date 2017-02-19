8b5c0a309317409ad273c8cb4cc8649e60972a0b
IwanHB <iwanhb@yandex.com>
2017-02-19 23:15:08 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,44 @@ namespace Uge1
 {
     class Program
     {
+
+        public class Timer
+        {
+            private readonly System.Diagnostics.Stopwatch stopwatch
+              = new System.Diagnostics.Stopwatch();
+            public Timer() { Play(); }
+            public double Check() { return stopwatch.ElapsedMilliseconds / 1000.0; }
+            public void Pause() { stopwatch.Stop(); }
+            public void Play() { stopwatch.Start(); }
+        }
+
+        public static double Opgave1Hjælpemetode(String inputstring, int rows, int columns) {
+
+            double dummy = 1.0;
+            string[] labyrinthDimensions = inputstring.Split('x');
+            int numberOfRows = Convert.ToInt32(labyrinthDimensions[0]) * rows;
+            int numberOfColumns = Convert.ToInt32(labyrinthDimensions[1]) * columns;
+            char[,] labyrinthCharacters = new char[numberOfRows, numberOfColumns];
+            return dummy;
+        
+        }
+
+        public static double Opgave1Hovedmetode(String inputstring, int rows, int columns) {
+
+            int n = 10;
+            int count = 1000000;
+            double dummy = 0.0;
+            for (int j = 0; j < n; j++)
+            {
+                Timer t = new Timer();
+                for (int i = 0; i < count; i++)
+                    dummy += Opgave1Hjælpemetode(inputstring, rows, columns);
+                double time = t.Check() * 1e9 / count;
+                Console.WriteLine("{0,6:F1} ns", time);
+            }
+            return dummy;
+
+        }
         static void Main(string[] args)
         {
             
@@ -19,7 +57,21 @@ namespace Uge1
 
             using (StreamReader streamReader = new StreamReader(fs, Encoding.UTF8))
             {                
-                firstLine = streamReader.ReadLine();
+                firstLine = streamReader.ReadLine();                
+
+                // Opgave 1: Tidstagning på konvertering fra tekst til heltal og oprettelse af array:
+                Console.WriteLine("Opgave 1:");
+                Opgave1Hovedmetode(firstLine, 1, 1);
+
+                // Opgave 2: Tidstagnning når størrelsen på arrayet tidobles:
+                Console.WriteLine("Opgave 2:");
+                Opgave1Hovedmetode(firstLine, 10, 10);
+
+                // Opgave 3: Tidstagning når størrelsen på arrayet hundrededobles:
+                Console.WriteLine("Opgave 3:");
+                Opgave1Hovedmetode(firstLine, 100, 100);
+
+
                 //Omdan den første linje på formen 21x21 til et string-array kun med tallene indsat som strings
                 string[] labyrinthDimensions = firstLine.Split('x');
                 
